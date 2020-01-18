fd0028dd971b511111586e75ac094541b64baae0
jpic <jpic@yourlabs.org>
2020-01-18 13:29:28 +0100

    Fix directory for compose

---
+++ files/remove.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+for home in /root /home/*; do
+    [ -d $home/.yourlabs.compose ] || continue
+    pushd $home/.yourlabs.compose &> /dev/null
+    for project in *; do
+        [ -f $project/removeat ] || continue
+        if [[ $(date +%s) -gt $(<$project/removeat) ]]; then
+            pushd $project
+            docker-compose down --rmi all --volumes --remove-orphans
+            popd &> /dev/null
+            rm -rf $project
+        fi
+    done
+    popd &> /dev/null
+done
